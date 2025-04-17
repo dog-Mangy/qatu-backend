78442c4ccd3fbbe2d1e232d76d9c63b56725fb0b
Manuel Vasquez <manuel.vasquez@jala.university>
2025-04-17 04:59:44 -0400

    feat: add MySQL db context and domain repositories and entities for User, Store and Product

---
+++ Qatu.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qatu.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int StoreId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Category { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; } = 0.0m;
+        public int Stock { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Store Store { get; set; } = null!;
+    }
+}
