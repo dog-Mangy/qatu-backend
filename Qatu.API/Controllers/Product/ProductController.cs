ac913b83c328ac92e5a974d5e812c103d9a34874
Ruben Ipenza <ruben.ipenza@jala.university>
2025-04-24 15:26:33 -0400

    feat: add controller to create a product

--- Qatu.API/Controllers/Product/ProductController.cs
+++ Qatu.API/Controllers/Product/ProductController.cs
@@ -9,14 +9,17 @@ public class ProductsController : ControllerBase
     private readonly GetProductByIdUseCase _getProductById;
     private readonly UpdateProductPriceUseCase _updatePrice;
     private readonly UpdateProductStockUseCase _updateStock;
+    private readonly CreateProductUseCase _createProduct;
 
     public ProductsController(GetProductByIdUseCase getProductById,
                               UpdateProductPriceUseCase updatePrice,
-                              UpdateProductStockUseCase updateStock)
+                              UpdateProductStockUseCase updateStock,
+                              CreateProductUseCase createProduct)
     {
         _getProductById = getProductById;
         _updatePrice = updatePrice;
         _updateStock = updateStock;
+        _createProduct = createProduct;
     }
 
     [HttpGet("{id}")]
@@ -39,5 +42,13 @@ public class ProductsController : ControllerBase
     {
         var result = await _updateStock.ExecuteAsync(dto);
         return result ? NoContent() : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto command)
+    {
+        var product = await _createProduct.HandleAsync(command);
+
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
     }
 }
