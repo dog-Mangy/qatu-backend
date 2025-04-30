5a0b5889bc987c5e38bf8851dabace3e3d275a8f
Manuel Vasquez <manuel.vasquez@jala.university>
2025-04-30 10:41:30 -0400

    feat: small change Make Items nullable on PagedResult DTO

--- Qatu.Application/DTOs/PagedResult.cs
+++ Qatu.Application/DTOs/PagedResult.cs
@@ -2,7 +2,7 @@
 {
     public class PagedResult<T>
     {
-        public IEnumerable<T> Items { get; set; }
+        public IEnumerable<T>? Items { get; set; }
         public int Page { get; set; }
         public int PageSize { get; set; }
         public bool HasNext { get; set; }
