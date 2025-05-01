80296d10db45dc1110a5223f5958444fe7b5ab9c
Juan Manuel Vasquez Aldana <manuel.vasquez@jala.university>
2025-05-01 16:44:02 +0000

    Feature/10/product pagination implementation

---
+++ Qatu.Application/DTOs/PagedResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Qatu.Application.DTOs
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T>? Items { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public bool HasNext { get; set; }
+        public bool HasPrevious { get; set; }
+        public int NPages { get; set; }
+        public int NElements { get; set; }
+    }
+}
