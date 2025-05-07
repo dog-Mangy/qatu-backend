feeadb88b36341581ca9b6a8654759dd57232616
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-07 11:54:21 -0400

    feat: configure SwaggerGen and custom Swagger UI endpoint

--- Qatu.API/Program.cs
+++ Qatu.API/Program.cs
@@ -37,17 +37,21 @@ builder.Services.AddDbContext<QatuDbContext>(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseSwaggerUI();
+app.UseSwaggerUI(o => o.SwaggerEndpoint("/openapi/v1.json", "Swagger Demo"));
 
 // Product
 app.UseMiddleware<RouteMiddleware>();
