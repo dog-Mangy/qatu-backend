150a1e50c88d96c7e0c312981bd03cb0f01ad571
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-25 22:30:51 -0400

    feat: add ExceptionHandlingMiddleware

---
+++ Qatu.API/Middlewares/Chat/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Qatu.API.Middlewares.Chat
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ArgumentException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await context.Response.WriteAsync("An unexpected error occurred.");
+            }
+        }
+    }
+}
