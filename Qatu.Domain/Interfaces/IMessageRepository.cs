3ebbab6f9d77b07a357dc88593bfee07d706dd15
Ruben Ipenza <ruben.ipenza@jala.university>
2025-05-27 20:45:40 -0400

    feat: add CreateMessageAsync method

--- Qatu.Domain/Interfaces/IMessageRepository.cs
+++ Qatu.Domain/Interfaces/IMessageRepository.cs
@@ -4,6 +4,7 @@ namespace Qatu.Domain.Interfaces
 {
     public interface IMessageRepository
     {
+        Task<Message> CreateMessageAsync(Message message);
         Task<IEnumerable<Message>> GetMessagesByChatIdAsync(Guid chatId);
     }
 }
--- Qatu.Infrastructure/Persistence/Repositories/MessageRepository.cs
+++ Qatu.Infrastructure/Persistence/Repositories/MessageRepository.cs
@@ -14,6 +14,34 @@ namespace Qatu.Infrastructure.Persistence.Repositories
             _context = context;
         }
 
+        public async Task<Message> CreateMessageAsync(Message message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            var chatExists = await _context.Chats.AnyAsync(c => c.Id == message.ChatId);
+            if (!chatExists)
+            {
+                throw new ArgumentException("Chat does not exist.", nameof(message.ChatId));
+            }
+
+            var chat = await _context.Chats.FirstOrDefaultAsync(c => c.Id == message.ChatId);
+            if (chat?.BuyerId != message.SenderId && chat?.SellerId != message.SenderId)
+            {
+                throw new ArgumentException("Sender is not a participant in this chat.", nameof(message.SenderId));
+            }
+
+            message.Id = Guid.NewGuid();
+            message.SentAt = DateTime.UtcNow;
+
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return message;
+        }
+
         public async Task<IEnumerable<Message>> GetMessagesByChatIdAsync(Guid chatId)
         {
             return await _context.Messages
