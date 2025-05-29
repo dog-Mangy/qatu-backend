fc881d9cc1c1add0cf9af29a28d55eccf40eef77
Dog-Mangy <edwin.chacon@jala.university>
2025-05-29 18:23:01 -0500

    feat: add the request domain

---
+++ Qatu.Domain/Entities/Request.cs
@@ -0,0 +1,22 @@
+using System;
+
+using Qatu.Domain.Enums;
+
+namespace Qatu.Domain.Entities
+{
+    public class Request
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public RequestStatus Status { get; set; } = RequestStatus.Pending;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
